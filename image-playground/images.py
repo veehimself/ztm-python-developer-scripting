5f984d0c20afb74e8a11d62ed0dfc9b7355636b1
veehimself <126854220+veehimself@users.noreply.github.com>
2023-11-14 13:43:18 +0530

    lecture-2 complete

--- image-playground/images.py
+++ image-playground/images.py
@@ -10,3 +10,12 @@ print(img.format)
 filtered_img = img.convert('L')
 
 filtered_img.save('greyscalepikachu.png','png')
+filtered_img.show()
+crooked = filtered_img.rotate(90)
+crooked.save('upsidedownpikachu.png','png')
+resize = filtered_img.resize((300,300))
+resize.save('resizepikachu.png','png')
+box = (100,100,400,400)
+region = filtered_img.crop(box)
+region.save('croppikachu.png','png')
+
