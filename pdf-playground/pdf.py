dd6b0106929e5dbc1677a90777afb704864fa3e7
veehimself <126854220+veehimself@users.noreply.github.com>
2023-11-15 10:48:00 +0530

    lecture-5 complete

---
+++ pdf-playground/pdf.py
@@ -0,0 +1,11 @@
+import PyPDF2
+
+with open('./dummy.pdf','rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    print(len(reader.pages))
+    page = reader.pages[0]
+    page.rotate(90)
+    writer = PyPDF2.PdfWriter()
+    writer.add_page(page)
+    with open('tilt.pdf','wb') as new_file:
+        writer.write(new_file)
